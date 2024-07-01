9c0882bd053986154f7bcb33df1ccca2981af8ec
Adesina Akinyemi <59088328+adesinasam@users.noreply.github.com>
2024-07-01 17:38:53 +0100

    Update table_order.py

--- restaurant_management/restaurant_management/doctype/table_order/table_order.py
+++ restaurant_management/restaurant_management/doctype/table_order/table_order.py
@@ -506,14 +506,24 @@ class TableOrder(Document):
             if item.status == status_attending:
                 items_to_return.append(i.identifier)
 
+                item.status = "Completed"
+                item.ordered_time = frappe.utils.now_datetime()
+                item.save()
+
+                data_to_send.append(table.get_command_data(item))
+
+            elif item.status == "Completed":
+                items_to_return.append(i.identifier)
+                
                 item.status = "Delivered"
                 item.ordered_time = frappe.utils.now_datetime()
                 item.save()
 
                 data_to_send.append(table.get_command_data(item))
 
+
         self.reload()
-        self.synchronize(dict(status=["Delivered"]))
+        self.synchronize(dict(status=item.status))
 
         return self.data()
 
